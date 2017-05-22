57087b21baeb1eb3b8aa400b687fbe70ff824466
Tomáš Pekař <xpekar10@stud.fit.vutbr.cz>
2017-05-22 21:31:17 +0200

    TextDecoder final fix. Edge is not supported.

--- j3a/src/crypter.js
+++ j3a/src/crypter.js
@@ -366,32 +366,12 @@ Crypter.prototype.ArrayBufferToHexString = function (buffer) {
 };
 
 /**
- * @description Provides conversion from ByteArray to Hex string (source: MDN documantation)
+ * @description Provides conversion from ByteArray to Hex string
  * @param {ByteArray} buffer Input ByteArray
  * @returns {string} String
  */
 Crypter.prototype.ArrayBufferToString = function (buffer) {
-
-    return String.fromCharCode.apply(null, buffer);
-
-    /*
-    var hexCodes = [];
-    var view = new DataView(buffer);
-
-    for (var i = 0; i < view.byteLength; i += 4) {
-        // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-        var value = view.getUint32(i)
-        // toString(16) will give the hex representation of the number without padding
-        var stringValue = value.toString(16)
-        // We use concatenation and slice for padding
-        var padding = '00000000'
-        var paddedValue = (padding + stringValue).slice(-padding.length)
-        hexCodes.push(paddedValue);
-    }
-
-    // Join all the hex strings into one
-    return (hexCodes.join("")).toLocaleUpperCase();
-    */
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
 };
 
 /**
